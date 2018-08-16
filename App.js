9e940036b4db1c7dc601a2a53646b577319e163c
Luna <lucidlyluna@gmail.com>
2018-08-16 12:32:56 -0400

    [WIP] Add escrow screen
    WIP because I haven't figured out yet how to share the password and
    baseUrl with the MyCardScreen

--- App.js
+++ App.js
@@ -8,14 +8,21 @@ import {
 
 import {
   createMaterialTopTabNavigator,
+  createDrawerNavigator,
 } from 'react-navigation';
 
 import ScanScreen from './app/ScanScreen.js';
 import MyCardScreen from './app/MyCardScreen.js';
+import Escrow from './app/Escrow.js';
 
-const App = createMaterialTopTabNavigator({
+const Home = createMaterialTopTabNavigator({
   Scan: { screen: ScanScreen },
   'My Card': { screen: MyCardScreen },
+});
+
+const App = createDrawerNavigator({
+	Home: { screen: Home },
+	Escrow: { screen: Escrow },
 });
 
 AppRegistry.registerComponent('default', () => App);
---
+++ app/Escrow.js
@@ -0,0 +1,56 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+
+import EText from './EText.js';
+import EButton from './EButton.js';
+
+class Escrow extends Component {
+  static navigationOptions = {
+    drawerLabel: 'Escrow',
+  };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  completeEscrow() {
+    let url = 'TODO';
+    let password = 'TODO';
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        password,
+      }),
+    });
+  }
+  render() {
+    return <ScrollView contentContainerStyle={styles.padded}>
+      <EText>Escrow allows you to:</EText>
+      <EText>Recover your password if lost</EText>
+      <EText>Make shared contingency plans</EText>
+      <EText>Please someone other than me write this copy</EText>
+      <EText>More</EText>
+      <EButton onPress={this.completeEscrow.bind(this)}>
+        Escrow my card
+      </EButton>
+    </ScrollView>;
+  }
+}
+
+const styles = StyleSheet.create({
+  padded: {
+    padding: 12,
+  },
+});
+
+module.exports = Escrow;
+
